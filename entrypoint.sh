9778fec6450a19ea6da20f64e5f85e7ed3969dbd
Aditya Sharma <git@adi.run>
2020-09-04 20:50:26 -0400

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,4 +4,4 @@ if [ ! -e /dev/net/tun ]; then  mknod /dev/net/tun c 10 200; fi
 
 # Wait 5s for the daemon to start and then run tailscale up to configure
 /bin/sh -c "sleep 5; tailscale up --authkey=${TAILSCALE_AUTH} &
-exec /usr/bin/tailscaled --state=/tailscale/tailscaled.state
+exec /usr/bin/tailscaled --state=/tailscale/tailscaled.state"
